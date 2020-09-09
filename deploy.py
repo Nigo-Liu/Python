66ab6cd39842f573481430776cf1d726fcdf784e
Nigo_Liu 劉俊麟 <nigo_liu@leadtek.com.tw>
2020-09-09 16:27:48 +0800

    modify deploy.py file

--- deploy.py
+++ deploy.py
@@ -13,7 +13,7 @@ from PIL import Image
 # setting initial path at "/home/ubuntu/Desktop"
 os.chdir("/home/ubuntu/Desktop")
 
-# 
+# tk GUI
 window = tk.Tk()
 window.title('LEADTEK')
 window.geometry('800x600')
@@ -42,24 +42,45 @@ def deploy_seletcion():
     else:
         l.config(text='Deploy all components')
 
+client = docker.APIClient(base_url='unix://var/run/docker.sock')
 gdms_link = "" 
 harbor_link = ""
 netdata_link = ""
 
 def push_images():
-   print("To do something.") 
-    
-
+    client = docker.APIClient(base_url='unix://var/run/docker.sock')
+    os.chdir("/var/cache/images/")
+    pull_info = tk.StringVar()
+    for key,value in _varvalue.items():
+        if value == 1:
+            docker_exec = ["/usr/bin/docker", "load -i", key]
+            docker_command = " ".join(docker_exec)
+            check_output(docker_command, shell=True)
+            t = 1
+            while t < 30:
+                image_list = check_output("docker images", shell=True).decode('utf-8')
+                if str(key).replace('.tar','') in image_list:
+                    print ('image upload completed')
+                    for line in client.push('rtxws.com:8443/library/'+str(key).replace('.tar',''), stream=True):
+                        print(line)
+                        pull_info.set("image push completed.")
+                    break
+                else:
+                    t += 1
+    label4 = tk.Label(window, textvariable=pull_info, bg='white')
+    label4.pack(fill=X,anchor=S)
 def Readstatus(key):
-    print (_var.get(key))
     _var_obj = _var.get(key)
-    print (_var_obj.get())
+    print(_var_obj.get())
+    _varvalue[key]=_var_obj.get()
 
 def deploy_components():
     global gdms_link
     global harbor_link
     global netdata_link
     global _var
+    global _varvalue
+    # GDMS kvm deploy
     if (var1.get() == 1) & (var2.get() == 0) & (var3.get() == 0) :
         host_name = "gdms"
         host_list = check_output("virsh list --all | sed -n '3, 1p' | awk '{print $2}'", shell=True)
@@ -106,6 +127,7 @@ def deploy_components():
             else:
                 gdms_link = 'http://' + ip_address.rstrip("\n") + ':8080/GDMSWeb/'
                 label['text'] = gdms_link
+    # harbor deploy
     elif (var1.get() == 0) & (var2.get() == 0) & (var3.get() == 1) :
         os.chdir("/home/ubuntu/harbor-master")
         harbor_list = check_output('sudo docker-compose ps', shell=True).decode('utf-8')
@@ -117,15 +139,34 @@ def deploy_components():
             label3['text'] = harbor_link
             image_path = "/var/cache/images"
             image_list = listdir(image_path)
-            print(image_list)
             _var = dict()
+            _varvalue = dict()
+            # dynamic create image tar file to checkbutton
             for image in image_list:
                 _var[image] = tk.IntVar()
+                _varvalue[image] = 0
                 image = tk.Checkbutton(window, text=image, bg='white', variable=_var[image], onvalue=1, offvalue=0, command=lambda key=image: Readstatus(key))
-                image.pack(anchor=NW)  
+                image.pack(anchor=NW) 
+            # create push_image button
             push_btn = tk.Button(window, text='Push_images', bg='white', command=lambda :thread_it(push_images)).pack(fill=X,anchor=S)
-            print (_var.items())
-            
+        elif harbor in harbor_list:
+            harbor_link = 'https://rtxws.com:8443'
+            label3['text'] = harbor_link
+            image_path = "/var/cache/images"
+            image_list = listdir(image_path)
+            _var = dict()
+            _varvalue = dict()
+            # dynamic create image tar file to checkbutton
+            for image in image_list:
+                _var[image] = tk.IntVar()
+                _varvalue[image] = 0
+                print(type(tk.IntVar()))
+                print(type(_var[image]))
+                image = tk.Checkbutton(window, text=image, bg='white', variable=_var[image], onvalue=1, offvalue=0, command=lambda key=image: Readstatus(key))
+                image.pack(anchor=NW)
+            # create push_image button
+            push_btn = tk.Button(window, text='Push_images', bg='white', command=lambda :thread_it(push_images)).pack(fill=X,anchor=S)
+    # netdata(gpu & system monitor tool
     elif (var1.get() == 0) & (var2.get() == 1) & (var3.get() == 0) :
         check_output('./run_netdata.sh', shell=True)
         netdata_link = 'http://172.16.10.153:19999'
@@ -195,22 +236,39 @@ def show_endpoint():
                 t += 1
         if ip_address == "":
             gdms_link = 'Can`t get GDMS endpoint, please check your kvm configuration.'
-            label_e1['text'] = gdms_link
+            label['text'] = gdms_link
         else:
             gdms_link = 'http://' + ip_address.rstrip("\n") + ':8080/GDMSWeb/'
-            label_e1['text'] = gdms_link
+            label['text'] = gdms_link
     elif (var1.get() == 0) & (var2.get() == 0) & (var3.get() == 1) :
         os.chdir("/home/ubuntu/harbor-master")
         docker_list = check_output('sudo docker-compose ps', shell=True).decode('utf-8')
         print(docker_list)
         harbor_link = 'https://rtxws.com:8443'
-        label_e3['text'] = harbor_link
+        label3['text'] = harbor_link
     elif (var1.get() == 0) & (var2.get() == 1) & (var3.get() == 0) :
         netdata_link = 'http://172.16.10.153:19999'
-        label_e2['text'] = netdata_link
+        label2['text'] = netdata_link
     else:
         print("To do something.")
-    
+def show_images():
+    global _var
+    global _varvalue
+    harbor_link = 'https://rtxws.com:8443'
+    label3['text'] = harbor_link
+    image_path = "/var/cache/images"
+    image_list = listdir(image_path)
+    _var = dict()
+    _varvalue = dict()
+    # dynamic create image tar file to checkbutton
+    for image in image_list:
+        _var[image] = tk.IntVar()
+        _varvalue[image] = 0
+        image = tk.Checkbutton(window, text=image, bg='white', variable=_var[image], onvalue=1, offvalue=0, command=lambda key=image: Readstatus(key))
+        image.pack(anchor=NW)
+    # create push_image button
+    push_btn = tk.Button(window, text='Push_images', bg='white', command=lambda :thread_it(push_images)).pack(fill=X,anchor=S)
+
 var1 = tk.IntVar()
 var2 = tk.IntVar()
 var3 = tk.IntVar()
@@ -232,32 +290,20 @@ c3.pack(anchor=SW)
 #Button
 deploy_btn = tk.Button(window, text='DEPLOY', bg='white', command=lambda :thread_it(deploy_components)).pack(fill=X,anchor=S)
 delete_btn = tk.Button(window, text='DELETE', bg='white', command=lambda :thread_it(delete_components)).pack(fill=X,anchor=S)
+endpoint_btn = tk.Button(window, text='Endpoint', bg='white', command=lambda :thread_it(show_endpoint)).pack(fill=X,anchor=S)
+showimages_btn = tk.Button(window, text='Docker_images_list', bg='white', command=lambda :thread_it(show_images)).pack(fill=X,anchor=S)
 
 #Display_Label
-label = tk.Label(window) 
+label = tk.Label(window, bg='white') 
 label.pack(fill=X,anchor=S)
 label.bind("<Button-1>", lambda e: endpoint(gdms_link))
 
-label2 = tk.Label(window)
+label2 = tk.Label(window, bg='white')
 label2.pack(fill=X,anchor=S)
 label2.bind("<Button-1>", lambda e: endpoint_netdata(netdata_link))
 
-label3 = tk.Label(window)
+label3 = tk.Label(window, bg='white')
 label3.pack(fill=X,anchor=S)
 label3.bind("<Button-1>", lambda e: endpoint_harbor(harbor_link))
-
-endpoint_btn = tk.Button(window, text='Endpoint', bg='white', command=lambda :thread_it(show_endpoint)).pack(fill=X,anchor=S)
-
-label_e1 = tk.Label(window) 
-label_e1.pack(fill=X,anchor=S)
-label_e1.bind("<Button-1>", lambda e: endpoint(gdms_link))
-
-label_e2 = tk.Label(window)
-label_e2.pack(fill=X,anchor=S)
-label_e2.bind("<Button-1>", lambda e: endpoint_netdata(netdata_link))
-
-label_e3 = tk.Label(window)
-label_e3.pack(fill=X,anchor=S)
-label_e3.bind("<Button-1>", lambda e: endpoint_harbor(harbor_link))
 
 window.mainloop()
